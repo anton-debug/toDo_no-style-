2468ca937e4f2ebed2b59d9010848d1bf6fec573
anton <soldatoff27@gmail.com>
2020-04-15 00:46:42 +0300

    Изменение блока списка дел на список, добавление доп. классов с преф. -js

--- js/javascript.js
+++ js/javascript.js
@@ -2,17 +2,17 @@ $(document).ready(function(){
 
     function initialState() {
         if (localStorage.getItem('comments') == null) {
-            $('.left-section-empty').show();
+            $('.js-items__clean').show();
         } else {
-            $('.todo__list').html(localStorage.getItem('comments'));
-            $('.left-section-empty').hide();
+            $('.js-list').html(localStorage.getItem('comments'));
+            $('.js-items__clean').hide();
         }
     }
 
     initialState();
 
     function addToStorage() {
-        let content = $('.todo__list').html();
+        let content = $('.js-list').html();
         console.log(content);
         localStorage.setItem('comments', content);
     }
@@ -20,72 +20,89 @@ $(document).ready(function(){
 
 
     function addTodo() {
-        let name = $('.name-form-work').val(),//Забирает то что написали в input
-            text = $('.form-descr').val(); // Забирает то что написали в textarea
+        let name = $('.js-form__work').val(),//Забирает то что написали в input
+            text = $('.js-form__descr').val(); // Забирает то что написали в textarea
 
         if (name && text) { //Проверяем написано ли что то в формах
            
-            $('.name-form-work').removeClass('error');
-            $('.form-descr').removeClass('error');
+            $('.js-form__work').removeClass('error');
+            $('.js-form__descr').removeClass('error');
             
             // $('.left-section-empty').hide(); //если написано, то скрываем "Список пуст..."
-            $('.todo__list').append(`
+            // $('.todo__list').append(`
             
-                <div class="left-block-list">
-                    <div class="business-items">
-                        <p class="left-section-text">
-                            ${name}
-                        </p>
+            //     <div class="left-block-list">
+            //         <div class="business-items">
+            //             <p class="left-section-text">
+            //                 ${name}
+            //             </p>
 
-                        <button class="clear-btn btn-clear"></button>
+            //             <button class="clear-btn btn-clear"></button>
 
-                        <button class="arrow btn-clear"></button>
+            //             <button class="arrow btn-clear"></button>
                         
-                    </div>
+            //         </div>
 
-                    <div class="descr-item">
-                        <p class="descr-item-coments">
-                            ${text}
-                        </p>
-                    </div>
-                </div>
+            //         <div class="descr-item">
+            //             <p class="descr-item-coments">
+            //                 ${text}
+            //             </p>
+            //         </div>
+            //     </div>
 
+            // `);
+            $('.js-list').append(`
+                <li class="left-block-list js-list_items">
+                    <article class="business-items js-article">
+                        <header class="header__list">
+                            <h2 class="left-section-text">
+                                ${name}
+                            </h2>
+
+                            <button class="clear-btn btn-clear js-clear__btn"></button>
+                            <button class="arrow btn-clear js-arrow"></button>
+                        </header>
+                    </article>
+                    <p class="descr-item js-coments_list">
+                        ${text}
+                    </p>
+                </li>
             `);
 
             /* Обнуляем/очищаем формы после добавления дела
             добавляя пустую строку в val('') */
-            $('.left-section-empty').hide();
+            $('.js-items__clean').hide();
 
-            name = $('.name-form-work').val('');
-            text = $('.form-descr').val('');
+            name = $('.js-form__work').val('');
+            text = $('.js-form__descr').val('');
 
             addToStorage();
 
         } else { //Если в формах ничего нет и нажали кнопку добавить, то добавляется клас error
-            $('.name-form-work').addClass('error');
-            $('.form-descr').addClass('error');
+            $('.js-form__work').addClass('error');
+            $('.js-form__descr').addClass('error');
         }
     }
 
     function deleteComment(item) {
         item.remove();
 
-        let items = $('.left-block-list');
+        let items = $('.js-list_items');
 
         addToStorage();
 
         if (items.length ==0) {
-            $('.left-section-empty').show();
+            $('.js-items__clean').show();
             localStorage.removeItem('comments');
         }
     }
 
     $('.button').on('click', addTodo);
 
-    $('body').on('click', '.clear-btn', function(event){
+    $('body').on('click', '.js-clear__btn', function(event){
         event.preventDefault();
 
-        let item = $(this).parents('.left-block-list');
+        let item = $(this).parents('.js-list_items');
 
         addToStorage();
 
@@ -98,8 +115,8 @@ $(document).ready(function(){
         arrow.slideToggle();
     }
 
-    $('body').on('click', '.arrow', function(){ 
-        let arrow = $(this).parents('.left-block-list').find('.descr-item');
+    $('body').on('click', '.js-arrow', function(){ 
+        let arrow = $(this).parents('.js-list_items').find('.js-coments_list');
         // $('.left-block-list').addClass('zeroHeight');
         collapseComment(arrow);
     })
